93897c86633c9f35abe605545e0b3e0c3daf2e11
Nimer <nimerel_sayed@yahoo.com>
2024-07-21 00:16:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,18 @@ const router = require('./routes');
 
 const app = express();
 app.use(cors({
-    origin: process.env.FRONTEND_URL, // this is the front-end URL
+    origin: ["https://project-react-mer-ne-commerce-front.vercel.app/"], // this is the front-end URL
+    method: ["POST","GET"];
     credentials: true, // enable cookies
 }));
 app.use(express.json());
 app.use(cookieParser())
 
 app.use("/api",router);
+
+app.get("/", (req,res) => {
+    res.json("Helo Tiger...");
+})
 
 const PORT = 8000 || process.env.PORT
 connectDB().then(()=>{
